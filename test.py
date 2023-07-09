1cf1ac60b926e38c61d43dcaa6f668cd45fac4d2
Leon <leon@leon.it>
2023-07-09 16:51:15 +0200

    second file

---
+++ test.py
@@ -0,0 +1,12 @@
+class Rectangle():
+    def __init__(self,w,h):
+        self.w = w
+        self.h = h
+
+    def area(self):
+        return self.w * self.h    
+
+
+kwadrat = Rectangle(2,2)
+a = kwadrat.area()
+print(a)
